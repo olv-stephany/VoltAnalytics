4c8b16f92258ef8fab456aebfbabd949af88db10
Stephan Oliveira <stephany16ce@outlook.com>
2026-01-04 13:38:46 -0300

    umas coisa ai

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 const HomePage = () => {
   return (
-    <div><Header/></div>
+    <div>
+        <Header/>
+        <HeroSection/>
+    </div>
   )
 }
 
---
+++ src/components/heroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div>
+            <div className='text m-2 text-white'>
+                <h1 className='font1 tracking-[3px] text-[48px]'>Dados que impulsionam a eficiência
+                    <br />energética <br /></h1>
+
+                <p className='text-end text-[24px] tracking-[1px] font2'>Em um cenário onde o uso consciente de recursos se torna cada vez mais essencial, o VoltAnalytics surge
+                    <br />como uma solução demonstrativa que <strong className='text-[#06BF85]'>evidencia como a tecnologia pode apoiar o monitoramento, a <br />
+                        análise e a otimização do consumo energético.</strong></p>
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
